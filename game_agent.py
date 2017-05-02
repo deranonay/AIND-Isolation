8f8f4ceb383bd694eb9b8ed4f0798a64935f5f0f
deran <deranonay@gmail.com>
2017-05-02 08:33:08 +0100

    more refactoring on scoring and added further comments

--- game_agent.py
+++ game_agent.py
@@ -9,6 +9,79 @@ class SearchTimeout(Exception):
     pass
 
 
+def get_open_moves(game, player):
+    """
+    :param game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells). 
+    :param player: object
+        A player instance in the current game (i.e., an object corresponding to
+        one of the player objects `game.__player_1__` or `game.__player_2__`.)
+    :return: tuple of player moves and inverted opponent moves normalised to 0-1 range.
+    """
+    max_move = 8
+    player_moves = len(game.get_legal_moves(player)) / max_move
+    opponent_moves = len(game.get_legal_moves(game.get_opponent(player))) / max_move
+    inv_opponent_moves = (float(1) / opponent_moves) if opponent_moves > 0 else float(1)
+    return player_moves, opponent_moves
+
+
+def get_distance_from_blanks(game, player):
+    """
+    :param game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells). 
+    :param player: object
+        A player instance in the current game (i.e., an object corresponding to
+        one of the player objects `game.__player_1__` or `game.__player_2__`.)
+    :return: inverted squared distance from centre of all blanks in the board
+    """
+    y, x = game.get_player_location(player)
+    blanks = game.get_blank_spaces()
+    sum_move = (0, 0)
+    for blank in blanks:
+        sum_move = (sum_move[0] + blank[0], sum_move[1] + blank[1])
+    blanks_centre_y = float(sum_move[0] / len(blanks))
+    blanks_centre_x = float(sum_move[1] / len(blanks))
+    sq_dist_from_blanks = float((y - blanks_centre_y) ** 2 + (x - blanks_centre_x) ** 2)
+    # sq_dist_from_blanks = (float(1) / sq_dist_from_blanks) if sq_dist_from_blanks > 0 else float(1)
+    return sq_dist_from_blanks
+
+
+def get_distance_to_centre(game, player):
+    """
+    :param game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells). 
+    :param player: object
+        A player instance in the current game (i.e., an object corresponding to
+        one of the player objects `game.__player_1__` or `game.__player_2__`.)
+    :return: inverted squared distance to the centre of the board
+    """
+    player_loc = game.get_player_location(player)
+    centre_loc = game.width / 2, game.height / 2
+    dist_to_centre = (player_loc[0] - centre_loc[0]) ** 2 + (player_loc[1] - centre_loc[1]) ** 2
+    inv_dist_to_centre = (float(1) / dist_to_centre) if dist_to_centre > 0 else 1
+    return dist_to_centre
+
+
+def get_distance_to_opponent(game, player):
+    """
+    :param game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells). 
+    :param player: object
+        A player instance in the current game (i.e., an object corresponding to
+        one of the player objects `game.__player_1__` or `game.__player_2__`.)
+    :return: inverted squared distance to the opponent
+    """
+    player_loc = game.get_player_location(player)
+    opponent_loc = game.get_player_location(game.get_opponent(player))
+    dist_to_opp = (player_loc[0] - opponent_loc[0]) ** 2 + (player_loc[1] - opponent_loc[1]) ** 2
+    inv_dist_to_opp = float(1) / dist_to_opp
+    return dist_to_opp
+
+
 def custom_score(game, player):
     """Calculate the heuristic value of a game state from the point of view
     of the given player.
@@ -33,28 +106,15 @@ def custom_score(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
+
     if game.is_loser(player):
         return float("-inf")
 
     if game.is_winner(player):
         return float("inf")
 
-    y, x = game.get_player_location(player)
-
-    max_move = 8
-    player_moves = len(game.get_legal_moves(player)) / max_move
-    opponent_moves = len(game.get_legal_moves(game.get_opponent(player))) / max_move
-    inv_opponent_moves = (float(1) / opponent_moves) if opponent_moves > 0 else float(1)
-
-    blanks = game.get_blank_spaces()
-    sum_move = (0, 0)
-    for blank in blanks:
-        sum_move = (sum_move[0] + blank[0], sum_move[1] + blank[1])
-    blanks_centre_y = float(sum_move[0] / len(blanks))
-    blanks_centre_x = float(sum_move[1] / len(blanks))
-    sq_dist_from_blanks = float((y - blanks_centre_y) ** 2 + (x - blanks_centre_x) ** 2)
-    inv_sq_dist_from_blanks = (float(1) / sq_dist_from_blanks) if sq_dist_from_blanks > 0 else float(1)
-    return inv_sq_dist_from_blanks + player_moves + inv_opponent_moves
+    player_moves, opponent_moves = get_open_moves(game, player)
+    return player_moves - opponent_moves
 
 
 def custom_score_2(game, player):
@@ -85,21 +145,13 @@ def custom_score_2(game, player):
     if game.is_winner(player):
         return float("inf")
 
-    blanks = game.get_blank_spaces()
-    sum_move = (0, 0)
-    for blank in blanks:
-        sum_move = (sum_move[0] + blank[0], sum_move[1] + blank[1])
-    blanks_centre_y = float(sum_move[0] / len(blanks))
-    blanks_centre_x = float(sum_move[1] / len(blanks))
+    sq_dist_from_blanks = get_distance_from_blanks(game, player)
+    sq_dist_from_blanks_opp = get_distance_from_blanks(game, game.get_opponent(player))
 
-    y, x = game.get_player_location(player)
-    sq_dist_from_blanks = float((y - blanks_centre_y) ** 2 + (x - blanks_centre_x) ** 2) / game.width**2*2
-    inv_sq_dist_from_blanks = (float(1) / sq_dist_from_blanks) if sq_dist_from_blanks > 0 else float(1)
+    dist_to_opp = get_distance_to_opponent(game, player)
+    dist_to_centre = get_distance_to_centre(game, player)
 
-    y2, x2 = game.get_player_location(game.get_opponent(player))
-    sq_dist_from_blanks_opp = float((y2 - blanks_centre_y) ** 2 + (x2 - blanks_centre_x) ** 2) / game.width**2*2
-
-    return sq_dist_from_blanks_opp * inv_sq_dist_from_blanks
+    return -sq_dist_from_blanks + sq_dist_from_blanks_opp - dist_to_centre - dist_to_opp
 
 
 def custom_score_3(game, player):
@@ -130,16 +182,12 @@ def custom_score_3(game, player):
     if game.is_winner(player):
         return float("inf")
 
-    player_loc = game.get_player_location(player)
-    opponent_loc = game.get_player_location(game.get_opponent(player))
-    dist_to_opp = (player_loc[0] - opponent_loc[0])**2 + (player_loc[1] - opponent_loc[1])**2
-    inv_dist_to_opp = float(1) / dist_to_opp
+    dist_to_opp = get_distance_to_opponent(game, player)
+    dist_to_centre = get_distance_to_centre(game, player)
+    dist_from_blanks = get_distance_from_blanks(game, player)
 
-    centre_loc = game.width/2, game.height/2
-    dist_to_centre = (player_loc[0] - centre_loc[0])**2 + (player_loc[1] - centre_loc[1])**2
-    inv_dist_to_centre = (float(1) / dist_to_centre) if dist_to_centre > 0 else 1
-
-    return inv_dist_to_centre + inv_dist_to_opp
+    total_score = dist_to_centre + dist_from_blanks*2
+    return float(1) / total_score if total_score > 0 else 1
 
 
 class IsolationPlayer:
@@ -172,8 +220,24 @@ class IsolationPlayer:
 
 
 class MinimaxBasedIsolationPlayer(IsolationPlayer):
+    """
+    Abstract IsolationPLayer class that defines some of the common functionality that is used by players that utilise 
+    minimax search algorithm with or without alpha-beta pruning. Concrete classes extending this abstract class should 
+    implement the search method with their choice of a search algorithm.
+    """
 
     def search(self, game, depth):
+        """
+        :param game: `isolation.Board`
+            An instance of `isolation.Board` encoding the current state of the
+            game (e.g., player locations and blocked cells).
+        :param depth: int
+            Depth is an integer representing the maximum number of plies to
+            search in the game tree before aborting
+        :return: (int, int)
+            The board coordinates of the best move found in the current search;
+            (-1, -1) if there are no legal moves
+        """
         raise NotImplementedError
 
     def get_move(self, game, time_left):
@@ -216,6 +280,7 @@ class MinimaxBasedIsolationPlayer(IsolationPlayer):
             # Default to a random legal move until we find a better one (in case timeout happens too quick)
             best_move = legal_moves[0]
 
+            # Perform iterative deepening search.
             depth = 1
             while self.search_depth >= depth:
                 best_move = self.search(game, depth)
@@ -223,38 +288,60 @@ class MinimaxBasedIsolationPlayer(IsolationPlayer):
 
         except SearchTimeout:
             # Handle any actions required at timeout, if necessary
-            # print('Timed out at depth {}'.format(depth))
             return best_move
 
         # Return the best move from the last completed search iteration
-        # raise NotImplementedError
-        # print('Final selected move is {}'.format(best_move))
         return best_move
 
-    def minimax_with_score(self, game, depth, alpha=float("-inf"), beta=float("inf"),
-                           apply_alphabeta=False):
+    def minimax_with_score(self, game, depth, alpha=float("-inf"), beta=float("inf"), apply_alphabeta=False):
+        """
+        :param game : isolation.Board
+            An instance of the Isolation game `Board` class representing the
+            current game state
+
+        :param depth : int
+            Depth is an integer representing the maximum number of plies to
+            search in the game tree before aborting
+
+        :param alpha : float
+            Alpha limits the lower bound of search on minimizing layers
+
+        :param beta : float
+            Beta limits the upper bound of search on maximizing layers
+        :param apply_alphabeta: bool
+            Flag that defines whether alpha beta pruning will be applied
+        :return: 
+        float
+            Score corresponding to the best move found in the current search
+        (int, int)
+            The board coordinates of the best move found in the current search;
+            (-1, -1) if there are no legal moves
+        """
+
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
 
-        is_maximising_player = game.active_player == self
+        # Return the score of the active player if bottom of the depth is reached or if there are no moves available
         legal_moves = game.get_legal_moves(game.active_player)
+        maximising = game.active_player == self
         if depth == 0 or len(legal_moves) == 0:
-            current_score = self.score(game, game.active_player if is_maximising_player else game.inactive_player)
-            # print('score is {}'.format(current_score))
-            return current_score, legal_moves[0] if len(legal_moves) > 0 else (-1, -1)
+            current_score = self.score(game, game.active_player if maximising else game.inactive_player)
+            return current_score, None
 
-        best_move = (float("-inf") if is_maximising_player else float("inf"), legal_moves[0])
-        get_better_move = lambda x, y: max(x, y, key=lambda a: a[0]) if is_maximising_player else min(x, y,
-                                                                                                      key=lambda a: a[
-                                                                                                          0])
-        updated_alpha = lambda a, x: max(a, x) if is_maximising_player else a
-        updated_beta = lambda b, x: min(b, x) if not is_maximising_player else b
+        # Define helper functions that perform the right action depending on if the active player is a maximising player
+        best_move = (float("-inf") if maximising else float("inf"), legal_moves[0])
+        get_better_move = lambda x, y: max(x, y, key=lambda a: a[0]) if maximising else min(x, y, key=lambda a: a[0])
+        updated_alpha = lambda a, x: max(a, x) if maximising else a
+        updated_beta = lambda b, x: min(b, x) if not maximising else b
 
+        # Pick the best move from available moves by iteratively calling minimax_with_score function
         for move in legal_moves:
             forecasted_game = game.forecast_move(move)
             (new_score, _) = self.minimax_with_score(forecasted_game, depth - 1, alpha, beta, apply_alphabeta)
             new_move = (new_score, move)
             best_move = get_better_move(new_move, best_move)
+
+            # Apply alpha-beta pruning if the flag is set
             if apply_alphabeta:
                 alpha = updated_alpha(alpha, best_move[0])
                 beta = updated_beta(beta, best_move[0])
